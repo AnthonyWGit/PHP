db5f38cf52149d8d5ac15495a12cb8eacc538f98
Wetzstein Anthony <anthony.wetzstein@gmail.com>
2023-03-20 11:59:50 +0100

    Email verification

---
+++ PHP 02/Exo15.php
@@ -0,0 +1,30 @@
+<?php
+function verifierEmail($email)
+{
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        $result = $email. " est un email valide.";
+        return $result;
+    }
+    else
+    {
+        $result = $email ." n'est pas un format valide.";
+        return $result;
+    }
+    if (filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        $result = $email. " est un email valide.";
+        return $result;
+    }
+    else
+    {
+        $result = $email ." n'est pas un format valide.";
+        return $result;
+    }
+}
+$result ="";
+$email = "elan@elan-formation.fr";
+echo verifierEmail($email);
+$email = "contact@elan";
+echo verifierEmail($email);
