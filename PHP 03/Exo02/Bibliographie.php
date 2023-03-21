c99c7a22707ed107a9844ff4e4c0f6740494b315
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-03-21 14:47:59 +0100

    -

--- PHP 03/Exo02/Bibliographie.php
+++ PHP 03/Exo02/Bibliographie.php
@@ -2,20 +2,24 @@
 class Bibliographie {
     private $livres;
     
-    public function __construct() {
-        $this->livres = array();
+    public function __construct() 
+    {
+        $this->livres = [];
     }
     
-    public function ajouterLivre($livre) {
+    public function ajouterLivre($livre)
+     {
         $this->livres[] = $livre;
     }
     
-    public function afficherBibliographie($auteur) {
+    public function afficherBibliographie($auteur) 
+    {
         $result ='Bibliographie de ' . $auteur . ':' . PHP_EOL;
         return $result;
-        
-        foreach ($this->livres as $livre) {
-            if ($livre->getAuteur() == $auteur) {
+        foreach ($this->livres as $livre) 
+        {
+            if ($livre->getAuteur() == $auteur)
+            {
                 echo '- ' . $livre . PHP_EOL;
             }
         }
