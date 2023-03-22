77fc3209163f8a6cae69ec68bce06b6ec37a23af
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-03-22 15:40:23 +0100

    Renomer acteur test index sans affichage réussi

--- PHP 03/Exo03/Role.php
+++ PHP 03/Exo03/Role.php
@@ -1,12 +1,13 @@
 <?php
 class Role 
 {
-    public string $name;
-    public Acteur $acteurs;
-    public function __construct($name, Acteur $acteurs)
+    public string $_nom;
+    public Acteur $_acteur;
+    public function __construct($name, Acteur $acteur)
     {
-        $this->name = $name;
-        $this->acteurs->addRole($this);
+        $this->_acteur = $acteur;
+        $this->_nom = $name;
+        $this->_acteur->addRole($this);
     }
 }
 ?>
