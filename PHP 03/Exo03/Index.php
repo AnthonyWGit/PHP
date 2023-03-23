952a1368b649aeebb5c19ce93b0b5be8326ed9a4
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-03-23 09:53:51 +0100

    oubli d'un echo dans index, ajout

--- PHP 03/Exo03/Index.php
+++ PHP 03/Exo03/Index.php
@@ -11,6 +11,9 @@ $result ="";
 //Instancier Real
 $zackSnyder = new Realisateur("Snyder","Zack", "homme", "1966");
 $robertTownsend = new Realisateur("Townsend", "Robert", "homme", "1901");
+//instancier Role
+$superman = new Role("Superman");
+$LtJohnJDunba = new Role (" Lt. John J. Dunba");
 //instancier acteurs
 $kevinCostner = new Acteur("Costner", "Kevin", "homme", "1955", $LtJohnJDunba);
 $henryCavill = new Acteur("cavill", "henry", "homme", "1983", $superman);
@@ -19,13 +22,11 @@ $tomWelling = new Acteur("welling", "tom", "homme", "1977", $superman);
 $meteorman = new Film("Meteor Man", "1993", 100,"string blabla²", "Comédie", $robertTownsend);
 $justiceLeague = new Film("Justice League", "2017", 120, "Lorem Ipsum dolor sit amet consectetur adip", "Super-Héro", $zackSnyder);
 $justiceLeague5 = new Film("Justice League", "2017", 120, "Lorem Ipsum dolor sit amet consectetur adip", "Super-Héro", $zackSnyder);
-//instancier Role
-$superman = new Role("Superman");
-$LtJohnJDunba = new Role (" Lt. John J. Dunba");
+
 //Instancier Casting
 
 //Affichage
 echo $justiceLeague->afficherReal();
 echo $zackSnyder->afficherFilmo();
 
-$henryCavill->afficherFilmoActeur();
+echo $henryCavill->afficherFilmoActeur();
