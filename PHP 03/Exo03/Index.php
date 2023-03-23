fe22b78763bfbf77a539c6704d6720616adb4e9f
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-03-23 09:43:22 +0100

    Commentaires et réécriture de l'index.php

--- PHP 03/Exo03/Index.php
+++ PHP 03/Exo03/Index.php
@@ -8,17 +8,23 @@ include 'Realisateur.php';
 
 
 $result ="";
-$billCosby = new Acteur("cosby", "bill", "homme", "1937");
+//Instancier Real
 $zackSnyder = new Realisateur("Snyder","Zack", "homme", "1966");
+$robertTownsend = new Realisateur("Townsend", "Robert", "homme", "1901");
+//instancier acteurs
+$billCosby = new Acteur("cosby", "bill", "homme", "1937");
 $henryCavill = new Acteur("cavill", "henry", "homme", "1983");
 $tomWelling = new Acteur("welling", "tom", "homme", "1977");
-$robertTownsend = new Realisateur("Townsend", "Robert", "homme", "1901");
+//instancier Films
 $meteorman = new Film("Meteor Man", "1993", 100,"string blabla²", "Comédie", $robertTownsend);
 $justiceLeague = new Film("Justice League", "2017", 120, "Lorem Ipsum dolor sit amet consectetur adip", "Super-Héro", $zackSnyder);
 $justiceLeague5 = new Film("Justice League", "2017", 120, "Lorem Ipsum dolor sit amet consectetur adip", "Super-Héro", $zackSnyder);
-$superman = new Role("Superman", $henryCavill);
-//$henryCavill->addRole($superman);
-//$tomWelling->addRole($superman);
+//instancier Role
+
+//Instancier Casting
+
+//Affichage
 echo $justiceLeague->afficherReal();
 echo $zackSnyder->afficherFilmo();
+
 $henryCavill->afficherFilmoActeur();
