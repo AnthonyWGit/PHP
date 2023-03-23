fcd890df612a42a1341e207ac80dfd38858be3da
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-03-23 10:42:23 +0100

    Creation Costner comme acteur+real

--- PHP 03/Exo03/Index.php
+++ PHP 03/Exo03/Index.php
@@ -10,23 +10,23 @@ include 'Realisateur.php';
 $result ="";
 //Instancier Real
 $zackSnyder = new Realisateur("Snyder","Zack", "homme", "1966");
-$robertTownsend = new Realisateur("Townsend", "Robert", "homme", "1901");
+$kevinCostnerR = new Realisateur("Costner", "Kevin", "homme", "1955");
 //instancier Role
 $superman = new Role("Superman");
 $LtJohnJDunba = new Role (" Lt. John J. Dunba");
 //instancier acteurs
-$kevinCostner = new Acteur("Costner", "Kevin", "homme", "1955", $LtJohnJDunba);
+$kevinCostnerA = new Acteur("Costner", "Kevin", "homme", "1955", $LtJohnJDunba);
 $henryCavill = new Acteur("cavill", "henry", "homme", "1983", $superman);
 $tomWelling = new Acteur("welling", "tom", "homme", "1977", $superman);
 //instancier Films
-$meteorman = new Film("Meteor Man", "1993", 100,"string blabla²", "Comédie", $robertTownsend);
 $justiceLeague = new Film("Justice League", "2017", 120, "Lorem Ipsum dolor sit amet consectetur adip", "Super-Héro", $zackSnyder);
 $justiceLeague5 = new Film("Justice League", "2017", 120, "Lorem Ipsum dolor sit amet consectetur adip", "Super-Héro", $zackSnyder);
-
+$danseAvecLesloups = new Film("Danse avec les Loups", "1990", 181, "DDDDDDDDDDDOOOOOOOOOOOOOOOO", "Western", $kevinCostnerR);
 //Instancier Casting
+$danseAvecLesloups = new Casting($LtJohnJDunba, $danseAvecLesloups, $kevinCostnerA);
 
 //Affichage
 echo $justiceLeague->afficherReal();
 echo $zackSnyder->afficherFilmo();
-
 echo $henryCavill->afficherFilmoActeur();
+echo $danseAvecLesloups->getCasting();
